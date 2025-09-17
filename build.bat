eace58dd63a94a99b6fdcef14b342187a7e87ef8
chrishealy8 <chrishealy8@gmail.com>
2025-09-17 12:35:02 +0800

    Use Glad as function loader

--- build.bat
+++ build.bat
@@ -6,10 +6,10 @@ if not exist build md build
 pushd build
 
 set Libs=user32.lib gdi32.lib opengl32.lib
-set CompileFlags=/nologo /W4 /Zi /FeRenderThread
+set CompileFlags=/nologo /W4 /Zi /I%ProjectRoot%\include /FeRenderThread
 @REM /DEBUG for Linker to combine debug info from multiple object files
 set LinkFlags=%Libs%
 
-cl %CompileFlags% %ProjectRoot%\src\main.cpp /link %LinkFlags%
+cl %CompileFlags% %ProjectRoot%\src\main.cpp %ProjectRoot%\src\glad.c /link %LinkFlags%
 popd
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,75 @@
 #define UNICODE
 #endif
 
+#include <stdio.h>
+
 #include <windows.h>
-#include <gl/gl.h>
+
+#include "glad/glad.h"
 
 typedef BOOL(WINAPI *wglSwapIntervalEXT_t)(int interval);
-// typedef int(WINAPI *wglGetSwapIntervalEXT_t)(void);
-
-static HGLRC gl_render_context;
 static wglSwapIntervalEXT_t wglSwapInterval = nullptr;
 
-float vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-     0.0f,  0.5f, 0.0f
-};
+static HGLRC gl_render_context;
 
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int) {
+    // SetProcessDPIAware();
+    // timeBeginPeriod(1);
+
+    // Register the window class
+    WNDCLASSEX wind_class = {};
+    wind_class.cbSize = sizeof(WNDCLASSEX);
+    wind_class.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC; // The first two flags together say: "WM_SIZE should trigger WM_PAINT"
+    wind_class.lpfnWndProc = WindowProc;
+    wind_class.hInstance = hInstance;
+    wind_class.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wind_class.hbrBackground = NULL; // We replace the window's entire contents every frame ourselves
+    wind_class.lpszClassName = L"ResponsiveRenderingWindow";
+
+    ATOM wnd_class_atom = RegisterClassEx(&wind_class);
+    if (!wnd_class_atom) {
+        OutputDebugString(L"Could not register Window Class");
+        return 1;
+    }
+
+    HWND hwnd = CreateWindowEx(
+        0,
+        wind_class.lpszClassName,
+        L"RenderThread",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+        NULL,
+        NULL,
+        hInstance,
+        NULL);
+
+    if (hwnd == NULL) {
+        OutputDebugString(L"Could not create Window");
+        return 1;
+    }
+    OutputDebugString(L"Window created successfully\n");
+
+    // UpdateWindow(hWnd);
+    ShowWindow(hwnd, SW_SHOW);
+
+    MSG msg;
+    // GetMessage returns 0 when it retrieves a WM_QUIT message
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        switch (msg.message) {
+        case WM_KEYDOWN:
+            if (msg.wParam == 'Q')
+                PostQuitMessage(0);
+        }
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
+}
+
+// TODO: All code interfacing with OpenGL will need to move to seperate thread
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     switch (uMsg) {
     case WM_CREATE: {
@@ -53,13 +107,18 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
         gl_render_context = wglCreateContext(hdc);
         wglMakeCurrent(hdc, gl_render_context);
 
+        if (!gladLoadGL()) {
+            OutputDebugString(L"Could not load OpenGL functions");
+            PostQuitMessage(1);
+            return 0;
+        }
+
         // MessageBoxA(0,(char*)glGetString(GL_VERSION), "OPENGL VERSION", 0);
 
-        wglSwapInterval = (wglSwapIntervalEXT_t)wglGetProcAddress("wglSwapIntervalEXT");
-        wglSwapInterval(1); // Enable V-Sync
+        // wglSwapInterval = (wglSwapIntervalEXT_t)wglGetProcAddress("wglSwapIntervalEXT");
+        // wglSwapInterval(1); // Enable V-Sync
 
         // glViewport(0, 0, 800, 600);
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
         return 0;
     }
@@ -75,7 +134,11 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
     case WM_PAINT: {
         BeginPaint(hwnd, NULL);
 
+        // render
+        // ------
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
         HDC hdc = GetDC(hwnd);
         SwapBuffers(hdc);
         // ReleaseDC(hwnd, hdc);
@@ -87,55 +150,4 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
     default:
         return DefWindowProc(hwnd, uMsg, wParam, lParam);
     }
-}
-
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int) {
-    // SetProcessDPIAware();
-    // timeBeginPeriod(1);
-
-    // Register the window class
-    WNDCLASSEX wind_class = {};
-    wind_class.cbSize = sizeof(WNDCLASSEX);
-    wind_class.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC; // The first two flags together say: "WM_SIZE should trigger WM_PAINT"
-    wind_class.lpfnWndProc = WindowProc;
-    wind_class.hInstance = hInstance;
-    wind_class.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wind_class.hbrBackground = NULL; // We replace the window's entire contents every frame ourselves
-    wind_class.lpszClassName = L"ResponsiveRenderingWindow";
-
-    RegisterClassEx(&wind_class);
-
-    HWND hwnd = CreateWindowEx(
-        0,
-        wind_class.lpszClassName,
-        L"RenderThread",
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-        NULL,
-        NULL,
-        hInstance,
-        NULL);
-
-    if (hwnd == NULL) {
-        OutputDebugString(L"Could not create Window");
-        return 1;
-    }
-    OutputDebugString(L"Window created successfully\n");
-
-    // UpdateWindow(hWnd);
-    ShowWindow(hwnd, SW_SHOW);
-
-    MSG msg;
-    // GetMessage returns 0 when it retrieves a WM_QUIT message
-    while (GetMessage(&msg, NULL, 0, 0)) {
-        switch (msg.message) {
-        case WM_KEYDOWN:
-            if (msg.wParam == 'Q')
-                PostQuitMessage(0);
-        }
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
-
-    return 0;
 }
