fc8f52e9018a183d37bdd60fe8bdb0270af3d221
chrishealy8 <chrishealy8@gmail.com>
2025-09-25 02:56:08 +0800

    Fix build

--- build.bat
+++ build.bat
@@ -7,7 +7,7 @@ pushd build
 
 set CompileFlags=/nologo /W4 /Zi /O2 /I%ProjectRoot%\include /FeRenderThread
 
-set cmd=cl %CompileFlags% %ProjectRoot%\src\main.cpp %ProjectRoot%\src\glad.c %ProjectRoot%\src\glad_wgl.c
+set cmd=cl %CompileFlags% %ProjectRoot%\src\main.c %ProjectRoot%\src\glad.c %ProjectRoot%\src\glad_wgl.c
 echo %cmd%
 %cmd%
 
